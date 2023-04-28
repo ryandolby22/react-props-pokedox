cb6347f255ee1cc5a1833ddb715a7340f6a83f12
ryandolby22 <115246771+ryandolby22@users.noreply.github.com>
2023-04-27 20:25:22 -0400

    Add files via upload

---
+++ src/Pokedex.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Pokecard from "./Pokecard";
+import "./Pokedex.css";
+
+/** Index of Pokemon: show individual cards.
+ *
+ * Props:
+ * - pokemon: array of pokemon: { id, name, type, base_experience }
+ * - exp: total of experience
+ * - isWinner: true/false
+ *
+ * */
+
+function Pokedex({ exp, isWinner, pokemon }) {
+  const winMessage = isWinner
+    ? <p className="Pokedex-winner">THIS HAND WINS!</p>
+    : null;
+
+  return (
+      <div className="Pokedex">
+
+        <h2 className="Pokedex-title">Pokedex</h2>
+        <div className="Pokedex-cards">
+          {pokemon.map(p => (
+              <Pokecard
+                  id={p.id}
+                  name={p.name}
+                  type={p.type}
+                  exp={p.base_experience}
+              />
+          ))}
+        </div>
+
+        <h4>Total experience: {exp}</h4>
+        {winMessage}
+
+      </div>
+  );
+}
+
+export default Pokedex;
